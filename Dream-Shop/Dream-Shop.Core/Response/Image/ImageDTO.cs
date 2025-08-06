f404cb0ea6e1bef1c876f4313d34250ac2480289
Tobias Runge <Tobias.Runge95@gmx.de>
2025-08-06 07:55:48 +0200

    Added ImageDTO

---
+++ Dream-Shop/Dream-Shop.Core/Response/Image/ImageDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Dream_Shop.Core.Response.Image;
+
+public class ImageDTO
+{
+    public Guid Id { get; set; }
+    public string FileName { get; set; }
+    public string DownloadUrl { get; set; }
+}
